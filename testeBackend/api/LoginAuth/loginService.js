75e6dafd91d9ad24d55969a97317030a44d511eb
Vinicius <viniciussmo16@gmail.com>
2019-05-29 14:06:11 -0300

    Verificação antes do Post

--- testeBackend/api/LoginAuth/loginService.js
+++ testeBackend/api/LoginAuth/loginService.js
@@ -2,5 +2,18 @@ const LoginService = require('./loginTask')
 
 LoginService.methods(['get', 'post', 'put', 'delete'])
 LoginService.updateOptions({ new: true })
+KiranCycle.before('post', verificacao)
+
+function verificacao(req, res, next){
+  KiranCycle.findOne({salaAtual: req.body.salaAtual}, function(err, user){
+    if(err){
+      return res.status(500).send('Falhou')
+    }if(!user){
+      return next()
+      //return res.status(404).send(next())
+    }
+    return res.status(200).send('Ja existe')
+  })
+}
 
 module.exports = LoginService
--- testeBackend/api/users/userService.js
+++ testeBackend/api/users/userService.js
@@ -2,5 +2,18 @@ const UserService = require('./userTask')
 
 UserService.methods(['get', 'post', 'put', 'delete'])
 UserService.updateOptions({ new: true })
+KiranCycle.before('post', verificacao)
+
+function verificacao(req, res, next){
+  KiranCycle.findOne({salaAtual: req.body.salaAtual}, function(err, user){
+    if(err){
+      return res.status(500).send('Falhou')
+    }if(!user){
+      return next()
+      //return res.status(404).send(next())
+    }
+    return res.status(200).send('Ja existe')
+  })
+}
 
 module.exports = UserService
