ebd4712271b5475d88dd4fd9b43c4ce4685c7680
Luiz Thomaz <luizthomazjunior@gmail.com>
2019-10-04 13:41:57 -0300

    Update server.js

--- testeBackend/config/server.js
+++ testeBackend/config/server.js
@@ -1,20 +1,21 @@
-const port = 5000
+const port = 8080
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
 const https = require('https')
+const http = require('http')
 const fs = require('fs')
 
 server.use(bodyParser.urlencoded({ extend: true }))
 server.use(bodyParser.json())
 
-https.createServer({
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem'),
-  passphrase: 'X9gxjujuba'
-}, server).listen(port, '0.0.0.0', function(){
-  console.log(`Https Backend is running on port ${port}.`)
+http.createServer({
+//  key: fs.readFileSync('./key.pem'),
+//  cert: fs.readFileSync('./cert.pem'),
+//  passphrase: 'X9gxjujuba'
+}, server).listen(port, function(){
+  console.log(`Http Backend is running on port ${port}.`)
 })
 
 server.use(function(req, res, next){
