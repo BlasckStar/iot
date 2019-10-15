98555cbb4975720a9448ba39454fc702f52cb5b2
Luiz Thomaz <luizthomazjunior@gmail.com>
2019-10-15 12:00:00 -0300

    Verificação no post
    Verifica se no banco de dados ja existe o token e caso exista ele nega o comando post

--- testeBackend/api/tokens/tokenService.js
+++ testeBackend/api/tokens/tokenService.js
@@ -2,6 +2,18 @@ const TokenService = require('./tokenTask')
 
 TokenService.methods(['get', 'post', 'put', 'delete'])
 TokenService.updateOptions({ new: true })
+TokenService.before('post', verifique)
 
-
+function verifique(req, res, next){
+  TokenService.findOne({token: req.body.token}, function(err, user){
+    if(err){
+      return res.status(500).send('Falhou')
+    }
+    if(!user){
+      return res.status(200).next()
+    }else{
+      return res.status(403).send('Usuario já existe')
+    }
+  })
+}
 module.exports = TokenService
--- testeBackend/api/users/userService.js
+++ testeBackend/api/users/userService.js
@@ -10,11 +10,9 @@ function verificacao(req, res, next){
       return res.status(500).send('Falhou')
     }if(!user){
       return next()
-      //return res.status(404).send(next())
+      return res.status(404).send(next())
     }
     return res.status(200).send('Ja existe')
   })
 }
-
-
 module.exports = UserService
